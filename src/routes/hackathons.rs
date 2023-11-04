47d1e4d8a2f6051a054ed414ee72e9212972f4df
Kanishk Sachdev <64576646+kensac@users.noreply.github.com>
2023-11-04 15:25:48 -0400

    added some more hackathon routes

--- src/routes/hackathons.rs
+++ src/routes/hackathons.rs
@@ -3,10 +3,11 @@ use std::vec;
 use axum::{
     extract::{Path, Query, State},
     response::Response,
-    routing::{get, post},
+    routing::{get, post, patch},
     Json, Router,
 };
 use chrono::FixedOffset;
+use hyper::StatusCode;
 
 use crate::{
     prisma::hackathon::{self, Data, UniqueWhereParam},
@@ -30,16 +31,17 @@ struct Params {
 async fn create_hackathon(
     State(app_state): State<AppState>,
     Json(body): Json<CreateHackathonEntity>,
-) -> Response<String> {
+) -> Result<Response<String>, StatusCode> {
+    //add event that also serves as check-in for hackathon
     match app_state
         .client
         .hackathon()
-        .create(body.name, body.start_time, body.end_time, true, vec![])
+        .create(body.name, body.start_time, body.end_time, false, vec![])
         .exec()
         .await
     {
-        Ok(_) => Response::new("Created hackathon successfully".to_string()),
-        Err(_) => Response::new("Failed to create hackathon".to_string()),
+        Ok(_) => Ok(Response::new("Created hackathon successfully".to_string())),
+        Err(_) => Err(StatusCode::BAD_REQUEST),
     }
 }
 
@@ -47,7 +49,7 @@ async fn create_hackathon(
 async fn get_hackathon(
     State(app_state): State<AppState>,
     Query(params): Query<Params>,
-) -> Result<Json<Vec<Data>>, ()> {
+) -> Result<Json<Vec<Data>>, StatusCode> {
     if params.active.is_some() {
         match app_state
             .client
@@ -57,12 +59,12 @@ async fn get_hackathon(
             .await
         {
             Ok(hackathons) => Ok(Json(hackathons)),
-            Err(_) => Err(()),
+            Err(_) => Err(StatusCode::NOT_FOUND),
         }
     } else {
         match app_state.client.hackathon().find_many(vec![]).exec().await {
             Ok(hackathons) => Ok(Json(hackathons)),
-            Err(_) => Err(()),
+            Err(_) => Err(StatusCode::NOT_FOUND),
         }
     }
 }
@@ -71,7 +73,7 @@ async fn get_hackathon(
 async fn get_hackathon_by_id(
     State(app_state): State<AppState>,
     Path(id): Path<i32>,
-) -> Result<Json<Vec<Data>>, ()> {
+) -> Result<Json<Vec<Data>>, StatusCode> {
     match app_state
         .client
         .hackathon()
@@ -81,9 +83,46 @@ async fn get_hackathon_by_id(
     {
         Ok(hackathons) => match hackathons {
             Some(hackathon) => Ok(Json(vec![hackathon])),
-            None => Err(()),
+            None => Err(StatusCode::NOT_FOUND),
         },
-        Err(_) => Err(()),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+#[axum::debug_handler]
+async fn delete_hackathon_by_id(
+    State(app_state): State<AppState>,
+    Path(id): Path<i32>,
+) -> Result<StatusCode, StatusCode> {
+    match app_state
+        .client
+        .hackathon()
+        .delete(UniqueWhereParam::IdEquals(id))
+        .exec()
+        .await
+    {
+        Ok(_) => Ok(StatusCode::NO_CONTENT),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+#[axum::debug_handler]
+async fn set_active_hackathon(
+    State(app_state): State<AppState>,
+    Path(id): Path<i32>,
+) -> Result<StatusCode, StatusCode> {
+    match app_state
+        .client
+        .hackathon()
+        .update(
+            UniqueWhereParam::IdEquals(id),
+            vec![hackathon::active::set(true)],
+        )
+        .exec()
+        .await
+    {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
 
@@ -91,6 +130,10 @@ pub async fn hackathon_get_router() -> Router {
     let state = get_app_state().await;
     Router::new()
         .route("/", post(create_hackathon).get(get_hackathon))
-        .route("/:id", get(get_hackathon_by_id))
+        .route(
+            "/:id",
+            get(get_hackathon_by_id).delete(delete_hackathon_by_id),
+        )
+        .route("/:id/active", patch(set_active_hackathon))
         .with_state(state)
 }
